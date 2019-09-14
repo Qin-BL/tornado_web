565294b1bcf9ddad2fa7dd08e7c17b91118bf03a
qinbilei <2528756899@qq.com>
2019-09-14 22:59:42 +0800

    fix bug

--- lib/redis.py
+++ lib/redis.py
@@ -5,7 +5,7 @@ from settings import DEFAULT_REDIS
 
 def gen_redis_client(conf=DEFAULT_REDIS):
     conf.update({"decode_responses": True})
-    return redis.StrictRedis(conf)
+    return redis.StrictRedis(**conf)
 
 
 rs = gen_redis_client()
