3f76874ad92347f57b8cbbf27436a8010e0a1dce
qinbilei <2528756899@qq.com>
2019-09-15 16:03:11 +0800

    add decorators

--- lib/decorators.py
+++ lib/decorators.py
@@ -1,11 +1,17 @@
 
 def model_to_dict(func):
     def wrapper(*args, **kwargs):
-        return func(*args, **kwargs).__dict__
+        res = func(*args, **kwargs).__dict__
+        del res['_sa_instance_state']
+        return res
     return wrapper
 
 
 def model_to_list(func):
     def wrapper(*args, **kwargs):
-        return [i.__dict__ for i in func(*args, **kwargs)]
+        res = func(*args, **kwargs)
+        for i in res:
+            i.__dict__
+            del i['_sa_instance_state']
+        return res
     return wrapper
